a8ffa1f662ceb4fdf29efdb0853848477853927d
marcinbroda <broda,marcin92@gmail.com>
2020-02-03 11:44:21 +0100

    new2020

--- script/script.js
+++ script/script.js
@@ -42,11 +42,13 @@ $(document).ready(function  (){
 		 	
 	});
 
+		   let window_scrollTop;
+
 	$( window ).scroll(function() {
+			    window_scrollTop = $(window).scrollTop();
 			var header_scroll =  $(".header");
 			var header_scroll_height = header_scroll.innerHeight();
 			var button_container = $(".button__container");
-			var window_scrollTop = $(window).scrollTop();
 			var button_container_height = button_container.innerHeight();
 
 		if ( header_scroll_height - (button_container_height + 70)   < window_scrollTop) {
@@ -55,16 +57,64 @@ $(document).ready(function  (){
 		}else{
 			button_container.removeClass("container__nav--active");
 		}
+			
 
-		 	
 	});
-
 
 		$("nav a").click(function(){
 			$("nav a").removeClass("nav__menu--active");
 			$(this).addClass("nav__menu--active");
 
 		});
+		//filter portfolio
+		$(".category_item").click(function () {
+			 $(".category_item").removeClass("hov");
+			 $(this).addClass("hov");
+
+			let category = $(this).attr("id");
+			   
+			if(category == "all"){
+				$(".all").addClass("hiden");
+				setTimeout(function(){
+					$(".all").removeClass("hiden");
+				}, 300);
+			} else {
+				$(".all").addClass("hiden");
+				setTimeout(function(){
+					$("." + category).removeClass("hiden");
+				}, 300);
+				console.log($(this));
+			}
+
+		});
+
+		//nawigacja pionowa
+
+		$( window ).scroll(function () {
+			let nav = $(".nav__list");
+			let header__nav = $('.header__nav');
+
+		if ( 65  < window_scrollTop) {
+			nav.addClass("nav__active-desktop");
+			header__nav.addClass("header__nav--active");
+			
+		}else{
+			nav.removeClass("nav__active-desktop");
+			header__nav.removeClass("header__nav--active");
+		}
+
+		});
+
+		/*$(window).scroll(function () {
+			let positonHash = $("#services");
+			let pos = positonHash.position();
+  			alert("Top: " + pos.top + " Left: " + pos.left);
+			console.log(positonHash);
+		});
+*/
+
+
+			
 
 });
 
