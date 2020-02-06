9139179dd73e2f967cff6a9a52872fe971a110bb
marcinbroda <broda,marcin92@gmail.com>
2020-02-06 13:42:21 +0100

    added new secion and small js

--- script/script.js
+++ script/script.js
@@ -4,31 +4,24 @@ $(document).ready(function  (){
 
 	$(document).ready(function(){
       $('.header__carousel--container').bxSlider({
-
       	auto: true,
       	pause: 4000,
-      	
       });
     });
 
+	let button =$(".button");
 
-	$(".button").on("click", function(e) {
+	button.on("click", function(e) {	
 	//do buttona
-		if ($(".button").hasClass("button-active") && ("button-after") && ("button-before") ) {
-			$(".button").removeClass("button-active"),
-			$(".button").removeClass("button-after"),
-			$(".button").removeClass("button-before");
+		if (button.hasClass("button-active") && ("button-after") && ("button-before") ) {
+			button.removeClass("button-active button-after button-before");
 		} else {
-			$(".button").addClass("button-active"),
-			$(".button").addClass("button-after"),
-			$(".button").addClass("button-before");
-			
+			button.addClass("button-active button-after button-before");
 		}
 
 	//do nawigacji
 			$("nav").toggleClass("nav__active");
 			$(".nav__cover").toggleClass("nav__cover--active")
-
 	});
 
 	$( window ).resize(function() {
@@ -42,7 +35,7 @@ $(document).ready(function  (){
 		 	
 	});
 
-		   let window_scrollTop;
+	let window_scrollTop;
 
 	$( window ).scroll(function() {
 			    window_scrollTop = $(window).scrollTop();
@@ -52,13 +45,12 @@ $(document).ready(function  (){
 			var button_container_height = button_container.innerHeight();
 
 		if ( header_scroll_height - (button_container_height + 70)   < window_scrollTop) {
-				button_container.addClass("container__nav--active");
+			button_container.addClass("container__nav--active");
 			
 		}else{
 			button_container.removeClass("container__nav--active");
 		}
 			
-
 	});
 
 		$("nav a").click(function(){
